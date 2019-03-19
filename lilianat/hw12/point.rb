aa4f97cce71b1012de11d789b35b7f2a68f66038
Liliana Galkin <lilianat@saltedge.com>
2019-03-19 15:27:33 +0200

    Add pp to gemfile

--- lilianat/hw12/point.rb
+++ lilianat/hw12/point.rb
@@ -1,12 +1,13 @@
 require "pry"
 require "rest-client"
 require "nokogiri"
+require "pp"
 
 data = RestClient.get('https://point.md/ru/').body
 page = Nokogiri::HTML(data)
 
 keys    = page.css("div#post-list-container article div h2").text.split("\n\n")[0..9].map! {|s| s.lstrip}
-links   = page.css("div#post-list-container figure img")[0..9].map! { |sr| sr.attr("src")}
+links   = page.css("div#post-list-container figure img")[0..9].map { |sr| sr.attr("src")}
 results = Hash[keys.zip links]
 
 puts results

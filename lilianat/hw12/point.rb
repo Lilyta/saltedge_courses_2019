57b867b3bcde8d2f3b820109824326a2f0368876
Liliana Galkin <lilianat@saltedge.com>
2019-03-19 18:54:53 +0200

    Refactoring hw12

--- lilianat/hw12/point.rb
+++ lilianat/hw12/point.rb
@@ -1,5 +1,6 @@
 require "pry"
 require "rest-client"
+require "open-uri"
 require "nokogiri"
 require "pp"
 
@@ -8,19 +9,24 @@ page = Nokogiri::HTML(data)
 
 keys    = page.css("div#post-list-container article div h2").text.split("\n\n")[0..9].map! {|s| s.lstrip}
 links   = page.css("div#post-list-container figure img")[0..9].map { |sr| sr.attr("src")}
+binding.pry
 results = Hash[keys.zip links]
 
 pp results
 
+=begindef download (url, dest)
+  open(url) do |u|
+    File.open(dest, 'wb') { |f| f.write(u.read) }
+  end
+end
+=end
 
 
-
-
-
-
-
-
-
-
-
+results.each do |keys, links| 
+	open(links) do |u|
+		keys.each do
+			File.open(name, "wb") { |f| f.write(u.read) }
+		end
+	end
+end
 
